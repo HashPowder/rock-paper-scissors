ec07e3020faa843d890ed0ca38b409b13d2d5b00
HashPowder <benbra667@gmail.com>
2023-04-25 11:50:43 +0200

    Add new function that plays a round of the game using player input and returns a winner

--- scripts.js
+++ scripts.js
@@ -3,8 +3,22 @@ let hand = ["rock", "paper", "scissors"]
 function getComputerChoice() {
     for (let i = 0; i < 1; i++) {
         let randomIndex = Math.floor(Math.random() * hand.length)
-        console.log(hand[randomIndex])
+        return hand[randomIndex]
     }
-    
 }
-getComputerChoice()
+
+
+
+ function playRound(playerSelection, computerSelection) {
+    if (playerSelection === hand[0] && computerSelection === hand[1]) {
+        console.log("You Lose! Paper beats Rock")
+    } else if (playerSelection === hand[1] && computerSelection === hand[2]) {
+        console.log("You Lose! Scissors beats Paper")
+    } else if (playerSelection === hand[2] && computerSelection === hand[0]) {
+        console.log("You Lose! Rock beats Scissors")
+    } else {
+        console.log("You Win!")
+    }
+ }
+
+playRound("rock", "paper")
